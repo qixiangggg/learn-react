0c6c670e4371a38c6c6193d86fa9f349364e0d83
DesmondChong <91426701+qixiangggg@users.noreply.github.com>
2025-03-15 23:44:56 +0800

    Complete chapter 2 exercise 7

--- 02. Data-Driven React/07. Aside - JS inside JSX/index.jsx
+++ 02. Data-Driven React/07. Aside - JS inside JSX/index.jsx
@@ -1,15 +1,26 @@
 import ReactDOM from 'react-dom/client';
 
 function App() {
-  const firstName = "Joe"
-  const lastName = "Schmoe"
+  const hours = new Date().getHours()
+  let timeOfDay
+
+  if (hours < 12) {
+    timeOfDay = "morning"
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon"
+  } else if (hours < 21) {
+    timeOfDay = "evening"
+  } else {
+    timeOfDay = "night"
+  }
   
   /**
-   * Challenge: finish off the h1 below so it says "Hello Joe Schmoe"
+   * Challenge: change the hard-coded "night" to display the
+   * text we determined from the logic above.
    */
-  
+
   return (
-    <h1>Hello {firstName} + " " + lastName</h1>
+    <h1>Good {timeOfDay}</h1>
   )
 }
 
