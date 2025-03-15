8eac9eb3fa06efb9e3bc1823f3e620a3cbac101f
DesmondChong <91426701+qixiangggg@users.noreply.github.com>
2025-03-15 23:16:17 +0800

    Complete chapter 2 exercise 3

--- 02. Data-Driven React/03. Travel Journal - Entry Component/App.jsx
+++ 02. Data-Driven React/03. Travel Journal - Entry Component/App.jsx
@@ -1,7 +1,10 @@
 import Header from "./components/Header"
-
+import Entry from "./components/Entry"
 export default function App() {
     return (
-        <Header />
+        <>
+            <Header/>
+            <Entry/>
+        </>
     )
 }
