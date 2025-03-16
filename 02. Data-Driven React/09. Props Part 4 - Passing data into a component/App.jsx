8b50f25cb11e59e8a212ad2dae9692054c847b82
DesmondChong <91426701+qixiangggg@users.noreply.github.com>
2025-03-16 12:35:49 +0800

    Complete exercise

--- 02. Data-Driven React/09. Props Part 4 - Passing data into a component/App.jsx
+++ 02. Data-Driven React/09. Props Part 4 - Passing data into a component/App.jsx
@@ -11,10 +11,27 @@ function App() {
             <Contact
                 img="./images/mr-whiskerson.png"
                 name="Mr. Whiskerson"
+                phone-number = "(212) 555-1234"
+                email = "mr.whiskaz@catnap.meow"
             />
-            <Contact />
-            <Contact />
-            <Contact />
+            <Contact 
+                img="./images/fluffykins.png"
+                name="Fluffykins"
+                phone-number = "(212) 555-2345"
+                email = "fluff@me.com"
+            />
+            <Contact 
+                img="./images/felix.png"
+                name="Felix"
+                phone-number = "(212) 555-4567"
+                email = "thecat@hotmail.com"
+            />
+            <Contact 
+                img="./images/pumpkin.png"
+                name="Pumpkin"
+                phone-number = "(0800) CAT KING"
+                email = "pumpkin@scrimba.com"
+            />
         </div>
     )
 }
