792ac584154226ae763fd1e1840d9583cd59cb58
DesmondChong <91426701+qixiangggg@users.noreply.github.com>
2025-03-17 00:37:38 +0800

    complete exercise

--- 02. Data-Driven React/23. Travel Journal - Pass object as props/components/Entry.jsx
+++ 02. Data-Driven React/23. Travel Journal - Pass object as props/components/Entry.jsx
@@ -3,13 +3,14 @@
  */
 
 export default function Entry(props) {
+    console.log(props)
     return (
         <article className="journal-entry">
             <div className="main-image-container">
                 <img 
                     className="main-image"
-                    src={props.img.src} 
-                    alt={props.img.alt}
+                    src={props.entry.img.src} 
+                    alt={props.entry.img.alt}
                 />
             </div>
             <div className="info-container">
@@ -18,11 +19,11 @@ export default function Entry(props) {
                     src="../images/marker.png" 
                     alt="map marker icon"
                 />
-                <span className="country">{props.country}</span>
-                <a href={props.googleMapsLink} target="_blank">View on Google Maps</a>
-                <h2 className="entry-title">{props.title}</h2>
-                <p className="trip-dates">{props.dates}</p>
-                <p className="entry-text">{props.text}</p>
+                <span className="country">{props.entry.country}</span>
+                <a href={props.entry.googleMapsLink} target="_blank">View on Google Maps</a>
+                <h2 className="entry-title">{props.entry.title}</h2>
+                <p className="trip-dates">{props.entry.dates}</p>
+                <p className="entry-text">{props.entry.text}</p>
             </div>
             
         </article>
